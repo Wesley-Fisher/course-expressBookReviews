1a17b94246f8443bc9b0962e12f422fab3c83c17
WF <wf@something.com>
2025-03-10 15:18:12 -0400

    Complete tasks 6-1 and 6-2

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,3 +1,5 @@
+
+// Note: treat key numbers for each book as ISBN value
 let books = {
       1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
       2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
