f5a231cdc27057675428a9b23f0c0e7cb0ee85ce
WF <wf@somewhere.com>
2025-03-11 09:40:58 -0400

    Complete 7-7 and 7-8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -18,19 +18,66 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    if (username.length == 0)
+    {
+        return false;
+    }
+
+    let filtered_users = users.filter((user) => (user.username == username && user.password === password));
+
+    return filtered_users.length == 1;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const password = req.body.password;
+  const username = req.body.username;
+
+  if(!password || !username)
+  {
+    return res.status(400).json({message: "Please provide a username and password"});
+  }
+
+  if(!authenticatedUser(username, password))
+    {
+        return res.status(400).json({message: "Failed to authenticate user"});
+    }
+
+    // Generate JWT access token
+    let accessToken = jwt.sign({
+        data: password
+    }, 'access', { expiresIn: 60 * 60 });
+    // Store access token and username in session
+    req.session.authorization = {
+        accessToken, username
+    }
+
+    return res.status(200).json({message: "Successfully logged-in user"});
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.session.authorization.username;
+  if (!username)
+  {
+    return res.status(400).json({message: "Error getting username from auth session"});
+  }
+
+  const isbn = req.params.isbn;
+  if(!isbn)
+  {
+    return res.status(400).json({message: "Please provide an ISBN"});
+  }
+
+  const review = req.query.review;
+  if(!review || review.length == 0)
+  {
+    return res.status(400).json({message: "Please provide a Review"});
+  }
+
+  books[isbn].reviews[username] = review;
+
+  return res.status(200).json(books[isbn]);
 });
 
 module.exports.authenticated = regd_users;
