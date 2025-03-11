a50f34ad805f4c02f4ba63a102e005fa66d97d93
WF <wf@somewhere.com>
2025-03-11 08:37:02 -0400

    Complete 6-3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,8 +35,22 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    if(!author)
+    {
+      return res.status(400).json({message: "Please provide an Author"});
+    }
+
+    let author_books = [];
+    let keys = Object.keys(books);
+    keys.forEach((key) =>
+    {
+        if (books[key].author === author)
+        {
+            author_books.push(books[key]);
+        }
+    });
+  return res.status(200).json(author_books);
 });
 
 // Get all books based on title
