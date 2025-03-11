033fa4677cab9b0cc829362f7ac89a7fbc06925a
WF <wf@somewhere.com>
2025-03-11 08:33:30 -0400

    Tidy some responses

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,15 +12,25 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.status(300).json(books);
+  return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
+  if(!isbn)
+  {
+    return res.status(400).json({message: "Please provide an ISBN"});
+  }
+
+  // Hack based on structure of code
+  if (isbn <= 0 || isbn > books.length)
+  {
+    return res.status(400).json({message: "ISBN not valid"});
+  }
+
   let book = books[isbn];
-  const book_data = JSON.stringify({book}, null, 4);
-  return res.status(300).json(book);
+  return res.status(200).json(book);
  });
   
 // Get book details based on author
